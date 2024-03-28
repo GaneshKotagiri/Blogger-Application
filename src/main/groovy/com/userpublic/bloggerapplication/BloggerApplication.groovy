073031e4465734d0d8ebf6e2f5ddf179dec6985a
Ganesh Kotagiri <123595702+Ganeshkotagiri-12@users.noreply.github.com>
2024-03-28 19:16:15 -0400

    project initialization and setup

---
+++ src/main/groovy/com/userpublic/bloggerapplication/BloggerApplication.groovy
@@ -0,0 +1,13 @@
+package com.userpublic.bloggerapplication
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BloggerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(BloggerApplication, args)
+	}
+
+}
